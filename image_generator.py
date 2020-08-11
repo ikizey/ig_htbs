50098ece18dfd25adeea25f9802c903366adbafa
ikizey <ikizey@gmail.com>
2020-08-11 20:04:01 +0300

    some docstrings. inner_centered_top fixed

--- image_generator.py
+++ image_generator.py
@@ -12,15 +12,16 @@ class InstaImgCreator:
 
         self.bg = Image.open(bg_path)
 
-        self._inner_box_coords = (110, 130, 971, 1168)
-        self.inner_left = self._inner_box_coords[0]
-        self.inner_width = self._inner_box_coords[2] - self._inner_box_coords[0]
-        self.inner_top = self._inner_box_coords[1]
-        self.inner_height = self._inner_box_coords[3] - self._inner_box_coords[1]
+        # self._inner_box_coords = (110, 130, 971, 1168)
+        self.inner_left = 110
+        self.inner_right = 971
+        self.inner_width = self.inner_right - self.inner_left
+        self.inner_top = 130
+        self.inner_bottom = 1168
+        self.inner_height = self.inner_bottom - self.inner_top
 
         self._original_text = text
         self.text = self._original_text
-        # self.text = self._original_text
 
     @property
     def text(self) -> str:
@@ -31,12 +32,19 @@ class InstaImgCreator:
         self._text = self.list_to_str(self.fit_text(text))
 
     def get_str_width(self, text: str) -> int:
+        """Returns width of text"""
         return self.font.getsize(text)[0]
 
     def get_str_height(self, text: str) -> int:
+        """Returns height of text"""
         return self.font.getsize_multiline(text)[1]
 
     def fit_text(self, text: str) -> list:
+        """Splits the text in lines, so it would fit in the inner box of the image
+
+        Returns list of strings. It gives the ability to use own line spacing.
+        """
+
         words = text.split(" ")
 
         # TODO DRY
@@ -61,32 +69,37 @@ class InstaImgCreator:
 
         return lines
 
-    def list_to_str(self, lines_of_text: List) -> str:
-        return "\n".join(lines_of_text)
+    def list_to_str(self, lines_of_text: List, sep: str = "\n") -> str:
+        """Just combines list of strings to single string"""
+        return sep.join(lines_of_text)
 
-    def centered_top(self) -> int:
-        """finds vertical position, to text to be vertically centered"""
+    def inner_centered_top(self) -> int:
+        """finds vertical coordinate, for text to be vertically centered inside the image"""
         text_height = self.get_str_height(self.text)
-        centered = (self.inner_height / 2 - text_height / 2) / 2
+        centered = (self.inner_height - text_height) / 2
         return centered
 
-    def imprint_text(self) -> None:
+    def imprint_text(self) -> Image.Image:
+        """Imprints text to image centered vertically inside inner box
+        Returns image
+        """
         draw = ImageDraw.Draw(self.bg)
         draw.multiline_text(
-            (self.inner_left, self.centered_top()),
+            (self.inner_left, self.inner_centered_top()),
             self.text,
             font=self.font,
             fill="black",
             spacing=-10,
         )
+        return self.bg
 
 
-def generate_text_from_csv() -> Generator[str]:
+def read_from_csv():
     with open("static/quotes.tsv", 'r') as quotes:
         lines = DictReader(quotes, delimiter='\t', quotechar='"')
         for line in lines:
             if line['post_date'] != '':
-                yield line['quote']
+                yield line['quote'], line['author']
 
 
 if __name__ == "__main__":
@@ -96,8 +109,7 @@ if __name__ == "__main__":
     GREEN_BG_PATH = "static/bg_green.jpeg"
 
     iic = InstaImgCreator(FONT_NAME, FONT_SIZE, GREEN_BG_PATH)
-    for word in generate_text_from_csv():
-        iic.text = word
-        iic.imprint_text()
-        iic.bg.show()
+    for quote, author in read_from_csv():
+        iic.text = quote
+        iic.imprint_text().show()
         break
