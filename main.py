0047772b12d385abf0c266244de1d0728a4ec11b
ikizey <ikizey@gmail.com>
2020-08-13 01:27:56 +0300

    refactor

--- main.py
+++ main.py
@@ -1,46 +1,55 @@
+from pathlib import Path
 from csv import DictReader
 
 from PIL import Image
-from PIL import ImageDraw
-
-from utils import MyInstaImgCreator
 
 
-def read_from_csv():
-    with open("static/quotes.tsv", 'r') as quotes:
+def read_from_csv(file_path):
+    with open(file_path, 'r') as quotes:
         lines = DictReader(quotes, delimiter='\t', quotechar='"')
         for line in lines:
-            if line['post_date'] != '':
-                yield line['quote'], line['author']
+            yield line['quote'], line['author'], line['post_date']
+
+
+def dict_of_images(path: Path) -> dict:
+    # # smart but fuck it, i only have 2
+    # files = [file for file in path.iterdir() if file.suffix == '.jpeg']
+    # backgrouds = [Image.open(str(file)).convert("RGBA") for file in files]
+    # # keys go like this : 0, 2, 4, 6 ...
+    # backgrouds = {
+    #     k: v for k, v in zip((z * 2 for z in range(len(backgrouds))), backgrouds)
+    # }
+
+    bgs_paths = (path.joinpath("bg_green.jpeg"), path.joinpath("bg_blue.jpeg"))
+    backgrounds = [Image.open(str(file)).convert("RGBA") for file in bgs_paths]
+    bgs = {0: backgrounds[0], 2: backgrounds[1]}
+    return bgs
 
 
 if __name__ == "__main__":
 
-    green_bg = Image.open("static/bg_green.jpeg").convert("RGBA")
-    blue_bg = Image.open("static/bg_blue.jpeg").convert("RGBA")
+    from utils import MyInstaImgCreator
 
-    import os
+    BASE_DIR = Path(__file__).resolve().parent  # Django? :)
 
-    base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    out = os.path.join(base_dir, 'out')
+    static = BASE_DIR.joinpath("static")  # backgorunds and csv with data
+    out = BASE_DIR.joinpath("out")
+
+    backgorunds = dict_of_images(static)
 
     iic = MyInstaImgCreator()
 
     counter = 0
-    f_counter = 0
-    for quote, author in read_from_csv():
+    for quote, author, date in read_from_csv(static.joinpath("quotes.tsv")):
         iic.quote = quote
         iic.author = author
 
-        ## use different backgrounds: 2 green 2 blue
-        if counter == 0:
-            iic.image = green_bg
-        elif counter == 2:
-            iic.image = blue_bg
-        elif counter > 3:
-            counter = -1
+        ## use different backgrounds: 2 green 2 blue repeat
+        if counter == 0 or counter == 2:
+            iic.image = backgorunds[counter]
+        counter = 0 if counter > 3 else counter + 1
 
-        iic.get_complete_image().save(f"out/img{f_counter}.jpeg", "JPEG2000")
-        counter += 1
-        f_counter += 1
+        out_file_path = str(out.joinpath(f"{date}.jpeg"))
+        iic.get_complete_image().save(out_file_path, "JPEG2000")
+
         break
