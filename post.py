0159411715db4a37b4fe1133eff5a322b9bd81af
ikizey <ikizey@gmail.com>
2020-08-13 19:43:16 +0300

    add auto caption generation

--- post.py
+++ post.py
@@ -1,8 +1,50 @@
 from datetime import datetime
 from pathlib import Path
+from random import choice, randint
 
 from dateutil import parser
 from instabot import Bot
+
+
+def generate_caption():
+    """Generates random set of tags for caption"""
+    prefix = "•\n" * 5
+    hashtag = "#"
+    tags = [
+        "success",
+        "entrepreneur",
+        "quoteoftheday",
+        "business",
+        "quote",
+        "quotes",
+        "entrepreneurship",
+        "successful",
+        "millionaire",
+        "motivational",
+        "wealth",
+        "dedication",
+        "billionaire",
+        "inspire",
+        "entrepreneurs",
+        "startup",
+        "instaquote",
+        "ambition",
+        "successquotes",
+        "quoteoftheday",
+        "lifecoach",
+        "happiness",
+        "joy",
+        "positivity",
+        "inspire",
+        "cool",
+        "nice",
+        "perfect",
+    ]
+    result_tags = set()
+    while len(result_tags) < randint(9, 15):
+        result_tags.add("".join([hashtag, choice(tags)]))
+    result = prefix + ", ".join(result_tags)
+    return result
 
 
 def is_today_file(file_name: str) -> bool:
@@ -22,7 +64,7 @@ if __name__ == "__main__":
         if file.suffix == ".png" and is_today_file(file.stem):
             bot = Bot()
             bot.login(username="", password="")
-            bot.upload_photo(str(file), caption=("#qotd"))
+            bot.upload_photo(str(file), caption=(generate_caption()))
             file.rename(posted.joinpath(file.name))  # to avoid double posts
             break
     else:
